99f3487fc5398a3f2e4885a0ef502fd909e2ad29
b4zz4 <bazza@riseup.net>
2016-03-22 18:57:03 -0300

    instalacion

--- instalar.sh
+++ instalar.sh
@@ -4,4 +4,5 @@ cd /tmp
 wget https://github.com/maquinas-libres/inkscape-tinycnc/archive/master.zip -O master.zip
 unzip master.zip
 cd inkscape-tinycnc-master
+mkdir -p ~/.config/inkscape/
 cp -r src/ ~/.config/inkscape/
