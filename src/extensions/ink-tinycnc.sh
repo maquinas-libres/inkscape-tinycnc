e9d2194dd25c19764bd2ffb15f3264319cd7d9a4
b4zz4 <bazza@riseup.net>
2016-03-23 16:13:28 -0300

    imprimir desde inkscape

--- src/extensions/ink-tinycnc.sh
+++ src/extensions/ink-tinycnc.sh
@@ -4,16 +4,10 @@
 #	AGPLv3 © Ernesto Bazzano
 #
 
-PUERTO=$(ls /dev/ttyUSB* | head -n1)
-echo $PUERTO
+PUERTO=$(ls /dev/ttyUSB* 2> /dev/null | head -n1)
+[ "$PUERTO" == "" ] && echo "Conecte el tinyCNC" > /dev/stderr && exit
 
-[ "$PUERTO" == "" ] && exit
-
-T=$(cat $1 | grep ^G1 | wc -l)
-N=0
-(sleep 3; $( dirname $0 )/tinycnc.py | grep ^G1 | cut -d'(' -f1 | cut -dF -f1 )| while read A; do
-	echo $((100/$T*$N)) > /dev/stderr
-	N=$((N=N+1))
+(sleep 3; $( dirname $0 )/tinycnc.py $@ | grep ^G1 | cut -d'(' -f1 | cut -dF -f1 )| while read A; do
 	echo $A
-	sleep 1
+	sleep .5
 done | cu -l $PUERTO -s 115200 > /dev/null
