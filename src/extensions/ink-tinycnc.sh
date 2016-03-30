c7e33e7d2116c7b484f9dd2496b92a4de587d289
b4zz4 <bazza@riseup.net>
2016-03-30 01:12:27 -0300

    detecta la aplicacion y mas rapido

--- src/extensions/ink-tinycnc.sh
+++ src/extensions/ink-tinycnc.sh
@@ -4,10 +4,12 @@
 #	AGPLv3 © Ernesto Bazzano
 #
 
+[ ! -x $(which cu) ] && echo "Tenes que instalar la aplicación \"cu\"\nsudo apt-get install cu" > /dev/stderr & exit
+
 PUERTO=$(ls /dev/ttyUSB* 2> /dev/null | head -n1)
 [ "$PUERTO" == "" ] && echo "Conecte el tinyCNC" > /dev/stderr && exit
 
 (sleep 3; $( dirname $0 )/tinycnc.py $@ | grep ^G1 | cut -d'(' -f1 | cut -dF -f1 )| while read A; do
         echo $A
-        sleep 1
+        sleep .3
 done | cu -l $PUERTO -s 115200 2> /dev/null > /dev/null
